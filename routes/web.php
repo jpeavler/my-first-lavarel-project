e9b126890ca1cbb743fa47e4506e581bc05c1fb6
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-17 16:20:23 -0600

    Added About and Services pages

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
