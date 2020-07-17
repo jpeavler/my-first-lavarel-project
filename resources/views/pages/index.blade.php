2e2ee602d11480b6523e5cbf4f083020baa3982d
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-17 15:55:50 -0600

    Created simple home page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', "MyApp")}}</title>
+
+    </head>
+    <body>
+        <h1>This is my first Laravel App</h1>
+        <p>Thanks to resources online, I've learned a lot quickly.</p>
+    </body>
+</html>
