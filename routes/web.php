f9195454edb7fb926595be0c27909dcce57228b1
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-18 16:01:18 -0600

    Generated Routes for Blog Posts

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
+
+Route::get('/users/{id}', function ($id) {
+    return "This is a user " . $id;
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts', 'PostsController');        //Generates all of the routes we need for
+                                                    //the post resource.
