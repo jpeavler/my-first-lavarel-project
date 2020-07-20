641cf64ca5f4ebfb89cc1e0caeaa98c8dcb7ce38
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-20 12:04:21 -0600

    Created table of all blogs that user has made on their home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,24 @@
                     @endif
                     <h3>Your Blog Posts</h3>
                     <a href = "/posts/create" class = "btn btn-primary">Create Post</a>
+                    @if(count($posts) > 0)
+                        <table class = "table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <th>{{$post->title}}</th>
+                                    <th><a href="/posts/{{$post->id}}/edit" class = "btn btn-default">Edit</a></th>
+                                    <th></th>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        You haven't made any posts yet.
+                    @endif
                 </div>
             </div>
         </div>
