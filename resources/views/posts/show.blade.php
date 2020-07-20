26a62a9ff4d28fb5fc60825c152241f80f920a7d
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-20 15:26:51 -0600

    Added display of image to individual blog views

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,15 @@
     <h1>{{$post->title}}</h1>
     <hr>
     <small>Written on {{$post->created_at}}</small>
-    <p>{{$post->body}}</p>
+    <div class = "row">
+        <div class = "col-md-4">
+            <img style = "width: 100%" src = "/storage/cover_images/{{$post->cover_image}}" alt = "{{$post->title}}">
+        </div>
+        <div class = "col-md-8">
+            <p>{{$post->body}}</p>
+        </div>
+    </div>
+    
     <hr>
     @if(!Auth::guest())
         @if(Auth::user()->id == $post->user_id)
