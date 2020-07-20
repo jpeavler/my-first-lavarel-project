09b6f86931954a5d8d735f6605cb8fa37aaf8d19
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-20 12:02:43 -0600

    Changed home link to be the user's home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
+                            <a class="nav-link" href="/home">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/about">About</a>
